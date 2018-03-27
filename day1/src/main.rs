402aec1bef0795e64f2fab7d2dd5946892a56e6b
houzhongjian <1556469348@qq.com>
2018-03-27 22:48:19 +0800

    基本操作

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,21 @@
+// use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    // let mut x = 1;
+    // x = 2;
+    // println!("x :{}", x);
+
+    // let x: i32 = 9;
+    // println!("{}", x);
+
+    // let mut x: i32 = 17;
+    // x = 8;
+    // let x = x;
+    // println!("{}", x);
+
+    Print(1)
+}
+
+fn Print(x: i32){
+    println!("x {}", x);
 }
