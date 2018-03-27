3449320497f7a47a3a00734f1fa75f3931f25357
houzhongjian <1556469348@qq.com>
2018-03-27 23:00:54 +0800

    基本操作

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -14,13 +14,19 @@ fn main() {
     // println!("{}", x);
 
     // Print(1)
-    Sum(1, 2);
+    // Sum(1, 2);
+    // let n = add_one(1);
+    // println!("{}", n);
 }
 
 // fn Print(x: i32){
 //     println!("x {}", x);
 // }
-
-fn Sum(x: i32, y: i32) {
-    println!("x + y = {}", x+y);
-}
+//
+// fn Sum(x: i32, y: i32) -> i32 {
+//     println!("x + y = {}", x+y);
+//     x + y;
+// }
+// fn add_one(x: i32) -> i32 {
+//     x + 1
+// }
